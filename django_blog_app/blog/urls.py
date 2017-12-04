b437ac9681e443a9885fee58b13a15b6d608fdd8
Treisy Jimenez <treisyj@thehangar.cr>
2017-12-04 12:52:16 -0600

    Create the template for the views

--- django_blog_app/blog/urls.py
+++ django_blog_app/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # post view
     url(r'^$', views.post_list, name='post_list'),
-    # url(r'^$', views.post_list, name='post_list'),
+
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
