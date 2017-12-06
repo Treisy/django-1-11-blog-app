ec20cb572e9406a4605c80e9cf10aac1b519d64f
Treisy Jimenez <treisyj@thehangar.cr>
2017-12-06 12:43:29 -0600

    Share posts by E-mail

--- django_blog_app/blog/views.py
+++ django_blog_app/blog/views.py
@@ -54,8 +54,8 @@ def post_share(request, post_id):
             message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
             send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
-        else:
-            form = EmailPostForm()
-        return render(request, 'blog/post/share.html', {'post': post,
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
                                                         'form': form,
                                                         'sent': sent})
--- django_blog_app/django_blog_app/settings.py
+++ django_blog_app/django_blog_app/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email configuration
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'treisyjimenez@gmail.com'
+# EMAIL_HOST_PASSWORD = '******'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
